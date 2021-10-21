730b0dcf96019523c239c5a1b1c1a4e7ff82bd41
jingug1004 <jingug1004@gmail.com>
2021-10-21 21:10:39 +0900

    토큰까지도 생성 완료. IntelliJ REST API 에서 테스트 성공 확인 완료. ^^

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.model.UserEntity;
+import com.example.demo.security.TokenProvider;
 import com.example.demo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TokenProvider tokenProvider;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
@@ -45,14 +49,39 @@ public class UserController {
         }
     }
 
+    /*
+POST http://localhost:8080/auth/signin
+
+HTTP/1.1 200
+Vary: Origin
+Vary: Access-Control-Request-Method
+Vary: Access-Control-Request-Headers
+Content-Type: application/json
+Transfer-Encoding: chunked
+Date: Thu, 21 Oct 2021 12:08:06 GMT
+Keep-Alive: timeout=60
+Connection: keep-alive
+
+{
+  "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0MDI4OTE4MjdjYTJjMDJmMDE3Y2EyYzA0Mzc1MDAwMCIsImlzcyI6ImRlbW8gYXBwIiwiaWF0IjoxNjM0ODE4MDg2LCJleHAiOjE2MzQ5MDQ0ODZ9.bbkaDetQoklNj0OrTBOC3J_FzYQBte-cHPauq3uPLS6mnxUsBLyd_H5WfVPM7yLz95um7cuzLUbx8PRZiPMpag",
+  "email": "hello@world.com",
+  "username": null,
+  "password": null,
+  "id": "402891827ca2c02f017ca2c043750000"
+}
+
+Response code: 200; Time: 984ms; Content length: 342 bytes
+    */
     @PostMapping("/signin")
     public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
         UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
 
         if (user != null) {
+            final String token = tokenProvider.create(user);
             final UserDTO responseUserDTO = UserDTO.builder()
                     .email(user.getEmail())
                     .id(user.getId())
+                    .token(token)
                     .build();
 
             return ResponseEntity.ok().body(responseUserDTO);
